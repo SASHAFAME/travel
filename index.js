df6dedb4cd35af42ffc4cc53ec75274447c0b6aa
SASHAFAME <sadsparrow@mail.ru>
2022-07-16 23:03:16 +0300

    feat: add slider

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav')
     const clo = document.querySelector('.header__nav-close');
-    const navItem = document.querySelector('.nav-items')
+    const navItem = document.querySelector('nav__close')
     const bg = document.querySelector('.menu-background');
+    const body = document.querySelector('body');
+    const lockPadding = document.querySelector('.lock-padding');
+    const popup = document.querySelector('.popup');
+    const popupLink = document.querySelector('#popupLink')
+    const buttonLogin = document.querySelector('.button_login');
 
 
     function openMenu() {
@@ -16,6 +21,10 @@
         bg.classList.remove('active');
     }
 
+    function switchPopup() {
+        nav.classList.remove('nav--visible');
+    }
+
     burger?.addEventListener('click', openMenu);
 
     clo?.addEventListener('click', openMenu);
@@ -23,3 +32,21 @@
     navItem?.addEventListener('click', closeMenu);
 
     bg?.addEventListener('click', closeMenu);
+
+    buttonLogin?.addEventListener('click', popupOpen);
+
+
+    function popupOpen() {
+        popup.classList.add('active');
+        bg.classList.add('active');
+    }
+
+    function popupClose() {
+        popup.classList.remove('active');
+        bg.classList.remove('active');
+    }
+
+    popupLink?.addEventListener('click', popupOpen);
+    popupLink?.addEventListener('click', switchPopup);
+    bg?.addEventListener('click', popupClose);
+    
